a9644472b63fac91956c0ef159b378bf00d173b7
KoalaSat <yv1vtrul@duck.com>
2023-10-28 22:20:56 +0200

    Tor SSL fix

--- tor/entrypoint.sh
+++ tor/entrypoint.sh
@@ -28,7 +28,7 @@ fi
 
 if [ "${SSL_CERT}" ] && [ "${SSL_KEY}" ]; then
 # Start socat with SSL
-	socat openssl-listen:${PUBLIC_PORT},reuseaddr,cert=/etc/socatorssl/certs/${SSL_CERT},key=/etc/socatorssl/certs/${SSL_KEY},verify=0,fork,keepalive SOCKS4A:127.0.0.1:${TOR_SITE}:${TOR_SITE_PORT},socksport=9050
+	socat openssl-listen:${PUBLIC_PORT},reuseaddr,cert=/etc/socatorssl/certs/live/satstralia.com/${SSL_CERT},key=/etc/socatorssl/certs/live/satstralia.com/${SSL_KEY},verify=0,fork,keepalive SOCKS4A:127.0.0.1:${TOR_SITE}:${TOR_SITE_PORT},socksport=9050
 fi
 
 
